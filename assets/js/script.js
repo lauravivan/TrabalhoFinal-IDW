22f3385061d7ba077e0c7b32879b7f5ccd4fa431
Laura <lauravivangoncalves@gmail.com>
2021-11-24 21:04:50 -0300

    Ajustes e melhorias

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,26 @@
-function showHomepageTabloidCatSide() {
-    tabloidLeftArrow.innerText = "";
-    picturesTabloid.innerHTML = `<img class="img-fluid vw-100" src="assets/img/cat.PNG">`
-    picturesTabloid.innerHTML += 
-                        `<div class="position-absolute pb-3">
-                            <a class="btn border border-dark rounded-pill fs-2 p-3 btn-see-more bolder-font-500 bg-color-lighter page-font text-decoration-none" href="cat.html" id="cats">Veja mais</a>
-                        </div>`
-    tabloidRightArrow.innerHTML = `<i class="bi bi-caret-right-fill fs-5 cursor-pointer" onclick="showHomepageTabloidDogSide()"></i>`;
+function userLogin() {
+    if (loggedUser.length > 0) {
+        document.getElementById("user-login").innerHTML = `<span class="dropdown-item bolder-font-700">Olá ${localStorage.getItem('LoggedUser')}</span>`;
+        document.getElementById("user-logout").innerHTML = `<span class="dropdown-item cursor-pointer" onclick="userLogout()">Sair</span>`;
+    } else {
+        document.getElementById("user-login").innerHTML = `<a class="dropdown-item" href="login.html">Fazer login</a>`;
+    }
 }
 
-function showHomepageTabloidDogSide() {
-    tabloidRightArrow.innerText = "";
-    picturesTabloid.innerHTML = `<img class="img-fluid vw-100" src="assets/img/dog.PNG">`
-    picturesTabloid.innerHTML += 
-                        `<div class="position-absolute pb-3">
-                            <a class="btn border border-dark rounded-pill fs-2 p-3 btn-see-more bolder-font-500 bg-color-lighter page-font text-decoration-none" href="dog.html" id="dogs">Veja mais</a>
-                        </div>`
-    tabloidLeftArrow.innerHTML = `<i class="bi bi-caret-left-fill fs-5 cursor-pointer" onclick="showHomepageTabloidCatSide()"></i>`
+function cartCount() {
+    if (cartProductsCount > 0) {
+        let cartIcon = document.getElementById("cart-icon");
+        cartIcon.innerHTML = `<span class="badge rounded-pill bg-dark position-absolute">${cartProductsCount}</span>`;
+    }
 }
 
 function showProducts(products, container) {
     for(let i = 0; i < products.length; i++) {
         container.innerHTML +=
             `<div class="col-md-4 col-lg-3 col-sm-6 col-12">
-                <div class="bg-light text-center pt-3 pb-2 product-info cursor-pointer" style="height: 36vw;">
-                    <img class="product-image img-fluid" id="image" onclick="goToProductPage(this.src)" src="${products[i].img}">
-                    <p class="text-uppercase pt-4 ps-2 pe-2 product-name bolder-font-700 page-font">${products[i].name}</p>
+                <div class="bg-light text-center pt-3 pb-2 opacity cursor-pointer container-fluid">
+                    <img class="img-fluid" id="image" onclick="goToProductPage(this.src)" src="${products[i].img}">
+                    <p class="text-uppercase m-2 product-name bolder-font-700 page-font">${products[i].name}</p>
                     <p>R$ ${products[i].price}</p>
                 </div>
             </div>`;
@@ -54,27 +50,26 @@ function discoverClickedProduct() {
 
 function showClickedProduct(clickedProduct) {
     let imageContainer = document.getElementById("clicked-product-img");
-    let nameContainer = document.getElementById("product-title-container");
-    let description = document.getElementById("product-description");
-    let price = document.getElementById("product-price");
+    let titleContainer = document.getElementById("clicked-product-title");
+    let descriptionContainer = document.getElementById("clicked-product-description");
+    let priceContainer = document.getElementById("clicked-product-price");
 
-    imageContainer.innerHTML += `<img class="img-fluid border border-dark border-5 w-75" src="${clickedProduct.img}">`;
-    nameContainer.innerHTML = `<h2 class="text-uppercase fs-1 bolder-font-700" id="product-title" name="${clickedProduct.name}">${clickedProduct.name}</h2>`;
-    description.innerText = `${clickedProduct.description}`;
-    price.innerText = `Preço: R$${clickedProduct.price}`
+    imageContainer.innerHTML += `<img class="img-fluid border border-dark border-5" src="${clickedProduct.img}">`;
+    titleContainer.innerHTML = `<h2 class="text-uppercase display-6 bolder-font-700" id="product-title" name="${clickedProduct.name}">${clickedProduct.name}</h2>`;
+    descriptionContainer.innerHTML = `<p class="h3 bolder-font-500">${clickedProduct.description}</p>`;
+    priceContainer.innerHTML = `<p class="h3 bolder-font-700">Preço: R$${clickedProduct.price}</p>`
 }
 
 function buyProduct() {
-    if (parseInt(localStorage.getItem("countage products")) >= 20) {
+    if (JSON.parse(localStorage.getItem('CartProductsCount')) >= 20) {
         document.getElementById("alert").innerHTML = 
         `<div class="alert alert-warning" role="alert"> O seu carrinho já está cheio!</div>`;
     } else {
         let productTitleElement = document.getElementById("product-title").getAttribute("name");
         let quantitySelected = document.getElementById("quantity").value;
-        countageProducts++;
-        localStorage.setItem("countage products", countageProducts);
-        let cartIcon = document.getElementById("cart-icon");
-        cartIcon.innerHTML = `<span class="badge rounded-pill bg-dark position-absolute">${countageProducts}</span>`;
+        cartProductsCount++;
+        localStorage.setItem('CartProductsCount', JSON.stringify(cartProductsCount));
+        cartCount();
         discoverProductToBuy(productTitleElement, quantitySelected);
     }
 }
@@ -93,65 +88,50 @@ function discoverProductToBuy(productTitleElement, quantitySelected) {
     }
 }   
 
-
-//aqui que começa o problema. Precisa da página de login pra começar a resolver
 function putProductInTheCart(product, quantity) {
-    let user = JSON.parse(localStorage.getItem('users'))
-    user[0].cart.push(
+    cartProducts.push(
         {
             product: product,
             quantity: quantity
         }
     );
+    localStorage.setItem('CartProducts', JSON.stringify(cartProducts));
 }
 
 function takeProductOutOfCart(id) {
-    let products = JSON.parse(localStorage.getItem('selected products'));
-    for (let i = 0; i < products.length; i++) {
-        if (parseInt(id) == i) {
-            delete products[i];
+    for (let i = 0; i < cartProducts.length; i++) {
+        if (i == id) {
+            delete cartProducts[i];
         }
     }
-    products = products.filter(function (i) {
+    cartProducts = cartProducts.filter(function (i) {
         return i;
     })
-    localStorage.setItem('selected products', JSON.stringify(products));
+    localStorage.setItem('CartProducts', JSON.stringify(cartProducts));
 
-    countageProducts--;
-    localStorage.setItem("countage products", countageProducts);
-    let cartIcon = document.getElementById("cart-icon");
-    cartIcon.innerHTML = `<span class="badge rounded-pill bg-dark position-absolute">${countageProducts}</span>`;
+    cartProductsCount--;
+    localStorage.setItem('CartProductsCount', JSON.stringify(cartProductsCount));
+    cartCount();
     location.reload();
 }
 
 function finalizeOrder() {
-    if (loggedIn == "true") {
+    if (loggedUser.length > 0) {
         window.open("payment-deliver-adress.html", "_self");
     } else {
         window.open("login.html", "_self");
     }
 }
 
-function userCardChoices() {
-    let userChoices = {
-        card: document.querySelector('input[name=card]:checked').value,
-        cardImg: document.querySelector('input[name=card-img]:checked').value
-    };
-
-    localStorage.setItem('chosen card', JSON.stringify(userChoices));
-}
-
 function registerUser() {
     let emailAdress = document.getElementById("email-adress").value;
     let firstName = document.getElementById("first-name").value;
-    let lastName = document.getElementById("last-name").value;
-    let cpf = document.getElementById("cpf").value;
-    let cellNumber = document.getElementById("cell-number").value;
     let password = document.getElementById("user-password").value;
     let passwordConfirmation = document.getElementById("user-confirmation-password").value;
+    let alertContainer = document.getElementById("alert");
     let userAlreadyExists = false;
     if (password != passwordConfirmation) {
-        document.getElementById("alert").innerHTML = 
+        alertContainer.innerHTML = 
         `<div class="alert alert-warning" role="alert">A sua senha está incorreta!</div>`;
     } else {
         for (let i = 0; i < users.length; i++) {
@@ -161,27 +141,127 @@ function registerUser() {
         }
 
         if (userAlreadyExists) {
-            document.getElementById("alert").innerHTML = 
+            alertContainer.innerHTML = 
             `<div class="alert alert-warning" role="alert">Esse usuário já existe! Por favor faça login ou crie uma nova conta!</div>`;
         } else {
-            let user = [{
+            users.push({
                 emailAdress: emailAdress,
                 firstName: firstName,
-                lastName: lastName,
-                cpf: cpf,
-                cellNumber: cellNumber,
                 password: password,
-                cart: [],
+                deliverAdress: [],
+                creditCard: [],
+                debitCard: [],
                 orders: []
-            }]
-            localStorage.setItem('users', JSON.stringify(user));
-            emailAdress = "";
-            firstName = "";
-            lastName = "";
-            cpf = "";
-            cellNumber = "";
-            password = "";
-            passwordConfirmation = "";
+            });
+            localStorage.setItem('Users', JSON.stringify(users));
+            alertContainer.innerHTML = 
+            `<div class="alert alert-warning" role="alert">Cadastro feito com sucesso!</div>`;
+            document.getElementById("email-adress").value = "";
+            document.getElementById("first-name").value = "";
+            document.getElementById("user-password").value = "";
+            document.getElementById("user-confirmation-password").value = "";
+
+            setTimeout(function() {
+                window.open("index.html", "_self");
+            }, 1000);
+        }
+    }
+}
+
+function login() {
+    let emailAdress = document.getElementById("email-adress").value;
+    let userPassword = document.getElementById("user-password").value;
+    let alertDiv = document.getElementById("alert");
+    alertDiv.innerText = "";
+    let userDoesntExist = true;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].emailAdress == emailAdress && (users[i].password == userPassword)) {
+            localStorage.setItem("LoggedUser", users[i].firstName)
+            userDoesntExist = false;
+            setTimeout(function() {
+                window.open("index.html", "_self");
+            }, 2000);
+        }
+    }
+    if (userDoesntExist) {
+        alertDiv.innerHTML = `<div class="alert alert-warning" role="alert">O seu e-mail ou a sua senha estão incorretos!</div>`;
+    }
+}
+
+function userLogout() {
+    document.getElementById("user-login").innerHTML = `<a class="dropdown-item" href="login.html">Fazer login</a>`;
+
+    document.getElementById("user-logout").innerText = "";
+    localStorage.setItem('LoggedUser', "");
+}
+
+function addDeliverAdress() {
+    let streetAdress = document.getElementById("street").value;
+    let homeNumber = document.getElementById("home-number").value;
+    let complement = document.getElementById("complement").value;
+    let district = document.getElementById("district").value;
+    let cep = document.getElementById("cep").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+
+    if(streetAdress.length > 0 && (homeNumber.length > 0) && (district.length > 0)
+                && (cep.length > 0) && (city.length > 0) && (state.length > 0)) {
+        for (let i = 0; i < users.length; i++) {
+            if (loggedUser == users[i].firstName) {
+                users[i].deliverAdress.push({
+                    streetAdress: streetAdress,
+                    homeNumber: homeNumber,
+                    complement: complement,
+                    district: district,
+                    cep: cep,
+                    city: city,
+                    state: state
+                })
+
+                localStorage.setItem('Users', JSON.stringify(users));
+            }
+        }
+    }
+}
+
+function userCardChoices() {
+    chosenCard.push({
+        card: document.querySelector('input[name=card]:checked').value,
+        cardImg: document.querySelector('input[name=card-img]:checked').value
+    });
+
+    localStorage.setItem('ChosenCard', JSON.stringify(chosenCard));
+}
+
+function addCardInformation() {
+    let cardName = document.getElementById("card-name").value;
+    let cardNumber = document.getElementById("card-number").value;
+    let cardCode = document.getElementById("card-code").value;
+    let expireCardDate = document.getElementById("expire-card-date").value;
+
+    if(cardName.length > 0 && (cardNumber.length > 0) && (cardCode.length > 0)
+                && (expireCardDate.length > 0)) {
+        for (let i = 0; i < users.length; i++) {
+            if (loggedUser == users[i].firstName) {
+                if (chosenCard.card == "credit card") {
+                    users[i].creditCard.push({
+                        cardName: cardName,
+                        cardNumber: cardNumber,
+                        cardCode: cardCode,
+                        expireCardDate: expireCardDate
+                    })
+                } else {
+                    users[i].debitCard.push({
+                        cardName: cardName,
+                        cardNumber: cardNumber,
+                        cardCode: cardCode,
+                        expireCardDate: expireCardDate
+                    })
+                }
+
+                localStorage.setItem('Users', JSON.stringify(users));
+            }
         }
     }
 }
@@ -306,16 +386,14 @@ let catProducts = [
     }
 ]
 
-let picturesTabloid = document.getElementById("pictures-tabloid")
-let tabloidRightArrow = document.getElementById("tabloid-right-arrow");
-let tabloidLeftArrow = document.getElementById("tabloid-left-arrow");
-
 let mainOffers = [catProducts[0], catProducts[3], catProducts[6], catProducts[7], dogProducts[0], dogProducts[2], dogProducts[3], dogProducts[4]];
 
-let countageProducts = JSON.parse(localStorage.getItem('countage products'));
+let cartProductsCount = JSON.parse(localStorage.getItem('CartProductsCount')) || 0;
 
-let loggedIn = localStorage.getItem('logged in');
+let chosenCard = JSON.parse(localStorage.getItem('ChosenCard')) || [];
 
-let cardChosen = JSON.parse(localStorage.getItem('chosen card')) || [];
+let users = JSON.parse(localStorage.getItem('Users')) || [];
 
-let users = JSON.parse(localStorage.getItem('users')) || [];
+let cartProducts = JSON.parse(localStorage.getItem('CartProducts')) || [];
+
+let loggedUser = localStorage.getItem('LoggedUser') || "";
